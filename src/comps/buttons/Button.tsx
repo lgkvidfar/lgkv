836c4af27183d1404c96366a6376327c21c553c8
lukas vidfar <dev@arcadian.se>
2023-05-08 14:26:13 +0400

    2023-05-08

---
+++ src/comps/buttons/Button.tsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx'
+import React, { ButtonHTMLAttributes } from 'react'
+
+export interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode
+}
+
+export const Button = (props: Props) => {
+  return (
+    <button {...props} className={clsx('px-[1rem] py-[0.5rem] rounded', props.className)}>
+      {props.children}
+    </button>
+  )
+}
+
+export default Button

85087f125f86933795606102dae0c2d6145e5c4f
lukas vidfar <dev@arcadian.se>
2023-05-08 14:32:47 +0400

    2023-05-08

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
 import { Button } from './comps'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <main className="h-screen w-full bg-dark">
       <Button className="bg-light">Me Button</Button>

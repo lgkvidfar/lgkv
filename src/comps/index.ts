d604d21d803dd113a8331e1375ad168c824a8368
lukas vidfar <dev@arcadian.se>
2023-05-09 11:35:33 +0400

    2023-05-09

--- src/comps/index.ts
+++ src/comps/index.ts
@@ -2,5 +2,6 @@ export { default as Button } from './buttons/Button'
 export { default as Pinned } from './pinned/Pinned'
 export { default as ScrollMarq } from './scroll-marq/ScrollMarq'
 export { default as Follow } from './follow/Follow'
+export { default as HorizScroll } from './horiz-scroll/HorizScroll'
 
 import '../globals.css'
